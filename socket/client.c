e961ccf376bec7e2f8a76006755554131b9539b0
yfp1178966910 <1178966910@qq.com>
2018-04-03 21:04:33 +0800

    2018.4.3

--- socket/client.c
+++ socket/client.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 
 #define SERV_IP "127.0.0.1"
-#define SERV_PORT 9527
+#define SERV_PORT 6666
 
 int main(void)
 {
@@ -13,23 +13,25 @@ int main(void)
     struct sockaddr_in serv_addr;
     char buf[BUFSIZ]; 
 
-    /*创建一个socket 指定IPv4 TCP*/
+    /*创建一个客户端的socket 指定IPv4 TCP*/
     sfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    /*默认绑定bind, 系统指定*/
 
-    /*初始化一个地址结构:*/
+    /* 设定服务器的socket的数据 */
     bzero(&serv_addr, sizeof(serv_addr));                       //清零
     serv_addr.sin_family = AF_INET;                             //IPv4协议族
-    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);    //指定IP 字符串类型转换为网络字节序 参3:传出参数
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);    //设定服务端IP 字符串类型转换为网络字节序 参3:传出参数
     serv_addr.sin_port = htons(SERV_PORT);                      //指定端口 本地转网络字节序
 
-    /*根据地址结构链接指定服务器进程*/
+    /*发起连接请求*/
     connect(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
     while (1) {
         /*从标准输入获取数据*/
         fgets(buf, sizeof(buf), stdin);
         /*将数据写给服务器*/
-        write(sfd, buf, strlen(buf));       //写个服务器
+        write(sfd, buf, strlen(buf));       //写给服务器
         /*从服务器读回转换后数据*/
         len = read(sfd, buf, sizeof(buf));
         /*写至标准输出*/
--- socket/server.c
+++ socket/server.c
@@ -12,28 +12,36 @@ int main(void)
 {
     int lfd, cfd;
     struct sockaddr_in serv_addr, clie_addr;
-    socklen_t clie_addr_len;
     char buf[BUFSIZ];
     int n, i;
-
+    
+    // 建立一个服务端的socket
     lfd = socket(AF_INET, SOCK_STREAM, 0);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERV_PORT); 
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
+    
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);    //设定服务端IP 字符串类型转换为网络字节序 参3:传出参数
+    // serv_addr.sin_addr.s_addr = inet_pton(SERV_IP);
+    //serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY 指定一个任意地址，且是数值类型
+    // INADDR_ANY = inet_addr("0.0.0.0")  将一个点分十进制转换成长整型
+    
+    // 绑定 IP 端口
     bind(lfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
+    // 设置同时连接数量
     listen(lfd, 128);
 
-    clie_addr_len = sizeof(clie_addr);
+    socklen_t clie_addr_len = sizeof(clie_addr);
+
+    // 阻塞等待客户端连接，成功返回与客户端连接的文件描述符
     cfd = accept(lfd, (struct sockaddr *)&clie_addr, &clie_addr_len);
 
     while (1) {
-        n = read(cfd, buf, sizeof(buf));
+        n = read(cfd, buf, sizeof(buf));  // 读取客户端传进来的数据
         for (i = 0; i < n; i++)
             buf[i] = toupper(buf[i]);
-        write(cfd, buf, n);
+        write(cfd, buf, n);  // 写给客户端数据
     }
 
     close(lfd);
